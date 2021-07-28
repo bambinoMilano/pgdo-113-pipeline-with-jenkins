6b38da43241863c1cb32e159198a6bc8f3372b0c
Sachidanand Beeharry <sach.beeharry@bnc.ca>
2021-07-28 18:37:24 -0400

    PGDO113 : Add PipelineJenkinsController

---
+++ pipeline-jenkins/src/main/java/com/bambino/pipeline/jenkins/pipelinejenkins/PipelineJenkinsController.java
@@ -0,0 +1,19 @@
+package com.bambino.pipeline.jenkins.pipelinejenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/api/v1")
+public class PipelineJenkinsController {
+
+	@GetMapping(value = "/welcome")
+	@ResponseBody
+	public String welcome() {
+		return "Welcome to PG DO - CI/CD Pipeline with Jenkins ";
+	}
+	
+}
