d10a92c769e8b1997cee97b6909f1b95d110102c
Sach <82278533+bambinoMilano@users.noreply.github.com>
2021-07-29 18:37:22 -0400

    Update PipelineJenkinsController.java

--- pipeline-jenkins/src/main/java/com/bambino/pipeline/jenkins/pipelinejenkins/PipelineJenkinsController.java
+++ pipeline-jenkins/src/main/java/com/bambino/pipeline/jenkins/pipelinejenkins/PipelineJenkinsController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api/v1")
 public class PipelineJenkinsController {
+	
 
 	@GetMapping(value = "/welcome")
 	@ResponseBody
