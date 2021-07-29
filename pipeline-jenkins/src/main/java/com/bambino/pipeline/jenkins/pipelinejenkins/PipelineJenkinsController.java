b41c93a21eb7f235a786b572840eaa5d2209d1f2
Sach <82278533+bambinoMilano@users.noreply.github.com>
2021-07-29 18:37:46 -0400

    Test build trigger

--- pipeline-jenkins/src/main/java/com/bambino/pipeline/jenkins/pipelinejenkins/PipelineJenkinsController.java
+++ pipeline-jenkins/src/main/java/com/bambino/pipeline/jenkins/pipelinejenkins/PipelineJenkinsController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class PipelineJenkinsController {
 	
+	
 
 	@GetMapping(value = "/welcome")
 	@ResponseBody
